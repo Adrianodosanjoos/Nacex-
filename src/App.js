816a64c20968591d34d753c8bdd518e71aeefd14
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-12-22 08:30:01 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import emailjs from '@emailjs/browser'
 
 function App() {
   const [name, setName] = useState('')
@@ -8,8 +9,27 @@ function App() {
 
   function sendEmail(e){
     e.preventDefault();
-    
-    alert("TEST")
+
+    if(name === '' || email === '' || message === ''){
+      alert("preencha todos os campos");
+    return; 
+    }
+
+    const templeteParams = {
+      from_name: name,
+      message: message,
+      email: email 
+    }
+
+    emailjs.send("service_2cnqh5t", "template_59d1rza", templeteParams, "dO7tFerEOHEsaFB8f")
+    .then((response) => {
+        console.log("EMAIL SENT", response.status, response.text)
+        setName('')
+        setEmail('')
+        setMessage('')
+    }, (err) => {
+         console.log("erro: ", err)
+    }) 
 }
   return (
     <div className="container">
@@ -19,7 +39,7 @@ function App() {
         <input 
           className="input"
           type="text"
-          placeholder="Digite seu nome"
+          placeholder="your name"
           onChange={(e) => setName(e.target.value)}
           value={name}
         />
@@ -27,19 +47,19 @@ function App() {
         <input 
           className="input"
           type="text"
-          placeholder="Digite seu email"
+          placeholder="your email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
 
         <textarea 
           className="textarea"
-          placeholder="Digite sua mensagem..."
+          placeholder="Enter your message..."
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
 
-        <input className="button" type="submit" value="Enviar" />
+        <input className="button" type="submit" value="To send" />
       </form>
 
     </div>
