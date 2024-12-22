2b03e025b4f17c383d75af0d78824e59d1592a49
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-12-22 09:36:58 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -31,8 +31,16 @@ function App() {
          console.log("erro: ", err)
     }) 
 }
+
+          
   return (
     <div className="container">
+     
+
+       <p className="p">A <span>NACEX</span> é uma empresa líder no segmento de transporte expresso em Portugal, Espanha, Andorra e Benelux. Faz parte do Grupo Logista distribuidor líder de produtos e serviços para o comércio local no sul da Europa.</p>
+       <p className="p">Oferecemos uma ampla gama de serviços nacionais, internacionais e de valor acrescentado que se adaptam às necessidades de entrega mais exigentes do mercado. A <span>NACEX</span> disponibiliza aos seus clientes ferramentas tecnológicas e sistemas de comunicação que estão na vanguarda do setor.</p>
+       <p className="p">O nosso objetivo consiste em desenvolver serviços que satisfaçam as expectativas dos nossos clientes. Satisfazemos as suas necessidades logísticas e de distribuição através do desenvolvimento constante dos recursos humanos e técnicos para garantir a melhoria contínua dos nossos serviços.</p>
+
       <h1 className="title">NaceX</h1>
 
       <form className="form" onSubmit={sendEmail}>
